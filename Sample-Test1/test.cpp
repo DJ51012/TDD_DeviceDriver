cf1edcdc1d32ee3f9b73c7090c7b2a7c2bb33129
Dongjin <triumph0510@gmail.com>
2024-07-05 09:16:15 +0900

    enable gMock

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,4 +1,7 @@
-#include "pch.h"
+#pragma once
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../TDD_DeviceDriver/DeviceDriver.cpp"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
